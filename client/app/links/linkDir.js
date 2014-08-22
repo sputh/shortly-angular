6c5d3d134c715947afadf827c1bc2a25f1ef3cb1
sputh <samantha.puth@gmail.com>
2014-08-21 19:41:49 -0700

    Redo Directive Implementation

--- client/app/links/linkDir.js
+++ client/app/links/linkDir.js
@@ -1,22 +1,14 @@
 angular.module('shortly.links.directives', [])
-
-.directive('linksObject', function () {
-  // Your code here
-  // Function: Show links to User
-  var link = function (scope, element, attrs) {
-    console.log("scope",scope);
-    console.log("element",element);
-    console.log("attrs", attrs);
-    console.log('url', scope.urlInDirective);
-    scope.url = scope.urlInDirective;
-  }
-
-  return {
-    restrict: 'EA',
-    templateUrl: 'app/links/linkDir.html',
-    link: link,
-    scope: {
-      urlInDirective: '='
+  .directive('linksList', function(){
+    var link = function(scope, element, attrs) {
+      scope.url = scope.dirUrl;
     }
-  }
-});
+    return {
+      restrict: 'E',
+      templateUrl: '/app/links/linkDir.html',
+      link: link,
+      scope: {
+        dirUrl: '='
+      }
+    }
+  });
